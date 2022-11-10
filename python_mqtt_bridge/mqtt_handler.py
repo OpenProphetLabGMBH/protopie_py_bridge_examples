aa1a9a03f405b5de0d8a0e3cc325ab684316f94b
Saurabh Datta <hi@dattasaurabh.com>
2022-11-10 13:46:45 +0100

    business logic made into a func

--- python_mqtt_bridge/mqtt_handler.py
+++ python_mqtt_bridge/mqtt_handler.py
@@ -41,6 +41,46 @@ def on_broker_disconnect(client, userdata, rc):
     ''' Callback func that fires on getting disconnected from a broker '''
     print('[MQTT] DIS-CONNECTED from BROKER with result code: ', rc, '!')
 
+def map_io(_mqtt_topic, _mqtt_payload, _protopie_msg_id, _protopie_value):
+    ''' A function that directs the inputs, based on the config file, to the output pattern '''
+    # For all the MQTT topics listed in the config file
+    for i in range(len(subs_topics_list)):
+        #  if one of the topics matches with our input MQTT topic
+        if subs_topics_list[i] == _mqtt_topic:
+            # then, pick and set the respective socketio msg value as the one for socketio emit method
+            _protopie_msg_id = emmission_msgids_list[i]
+            # if the associated payload in the config file,
+            # is not a <str> 'payload'
+            if subs_payloads_list[i] != 'payload' and emmission_values_list[i] != 'value':
+                # And if, the recieved payload is in the config file (expected payload)
+                if subs_payloads_list[i] == _mqtt_payload:
+                    # then, set the socketio emit value as the correcponding value for that payload,
+                    # from the config file
+                    _protopie_value = emmission_values_list[i]
+            # also, if the associated payload in the config file,
+            # for that received topic is <str> 'payload',
+            # it simply means, replay the received mqtt payload, as it is, as the socketio value.
+            if subs_payloads_list[i] == 'payload' and emmission_values_list[i] == 'value':
+                _protopie_value = _mqtt_payload
+    if _protopie_msg_id is not None and _protopie_value is not None:
+        if io.connected:
+            print(
+                '[SOCKET_IO] Relaying MessageId:\'' + _protopie_msg_id +
+                '\', Value:\'' + _protopie_value + '\' to ProtoPieConnect server')
+            io.emit('ppMessage', {'messageId': _protopie_msg_id, 'value': _protopie_value})
+        else:
+            print('')
+            print('Not connected to socketio server ...')
+            print('Hence not emitting ...')
+            print('But messageId:', _protopie_msg_id, 'value:', _protopie_value)
+            print('')
+    else:
+        print('')
+        print('ALERT: One of the required value for socketio trasnmission is None')
+        print('MessageId: ', _protopie_msg_id, ', Value: ', _protopie_value)
+        print('Not emitting ...')
+        print('')
+
 def on_message_from_broker(client, userdata, msg):
     ''' Callback func that fires when we receive a message from the broker '''
     # Relay from MQTT -> socketio for PrototPieConnect method
@@ -60,44 +100,8 @@ def on_message_from_broker(client, userdata, msg):
           mqtt_payload + '\' + from MQTT broker')
     # MAPPINGS (BUSINESS LOGIC):
     protopie_msg = None
-    protopie_val = None
-    # For all the MQTT topics listed in the config file
-    for i in range(len(subs_topics_list)):
-        #  if one of the topics matches with our input MQTT topic
-        if subs_topics_list[i] == mqtt_topic:
-            # then, pick and set the respective socketio msg value as the one for socketio emit method
-            protopie_msg = emmission_msgids_list[i]
-            # if the associated payload in the config file,
-            # is not a <str> 'payload'
-            if subs_payloads_list[i] != 'payload' and emmission_values_list[i] != 'value':
-                # And if, the recieved payload is in the config file (expected payload)
-                if subs_payloads_list[i] == mqtt_payload:
-                    # then, set the socketio emit value as the correcponding value for that payload,
-                    # from the config file
-                    protopie_val = emmission_values_list[i]
-            # also, if the associated payload in the config file,
-            # for that received topic is <str> 'payload',
-            # it simply means, replay the received mqtt payload, as it is, as the socketio value.
-            if subs_payloads_list[i] == 'payload' and emmission_values_list[i] == 'value':
-                protopie_val = mqtt_payload
-    if protopie_msg is not None and protopie_val is not None:
-        if io.connected:
-            print(
-                '[SOCKET_IO] Relaying MessageId:\'' + protopie_msg +
-                '\', Value:\'' + protopie_val + '\' to ProtoPieConnect server')
-            io.emit('ppMessage', {'messageId': protopie_msg, 'value': protopie_val})
-        else:
-            print('')
-            print('Not connected to socketio server ...')
-            print('Hence not emitting ...')
-            print('But messageId:', protopie_msg, 'value:', protopie_val)
-            print('')
-    else:
-        print('')
-        print('ALERT: One of the required value for socketio trasnmission is None')
-        print('MessageId: ', protopie_msg, ', Value: ', protopie_val)
-        print('Not emitting ...')
-        print('')
+    protopie_value = None
+    map_io(mqtt_topic, mqtt_payload, protopie_msg, protopie_value)
 
 
 mqtt_client.on_connect = on_broker_connect
--- python_mqtt_bridge/socket_io_handler.py
+++ python_mqtt_bridge/socket_io_handler.py
@@ -41,56 +41,50 @@ def on_disconnect():
     ''' On dis-connect, notify '''
     print("[SOCKET_IO] DIS-CONNECTED from ProtoPieConnect!")
 
-@io.on('ppMessage')
-def on_message(data):
-    ''' Callback func that fires when a socketio message is received '''
-    protopie_message_id = data['messageId']
-    protopie_value = data['value'] if 'value' in data else None
-    # [NOTE]: Both the messageId and the value received from ProtoPieConnect are apparently, ALWAYS of type <str>
-    print('[SOCKET_IO] Received a Message from ProtoPieConnect server:', protopie_message_id, ':', protopie_value)
+
+def map_io(_protopie_msg_id, _protopie_value, _mqtt_topic, _mqtt_payload):
+    ''' A function that directs the inputs, based on the config file, to the output pattern '''
     # Do a check if the received message is in our config list
-    if protopie_message_id in subs_msgids_list:
+    if _protopie_msg_id in subs_msgids_list:
         print('[SOCKET_IO] Received value exists in our config')
         # Relay from PrototPieConnect's socketio server -> MQTT broker a/c to business logic
         # MAPPINGS (BUSINESS LOGIC):
-        mqtt_topic = None
-        mqtt_msg = None
         # For all the socketio messageIds listed in the config file
         for i in range(len(subs_msgids_list)):
             #  if one of the messageIds matches with our input sockeio messageIds
-            if subs_msgids_list[i] == protopie_message_id:
+            if subs_msgids_list[i] == _protopie_msg_id:
                 # then, pick and set the respective mqtt topic value as the one for mqtt publish method
-                mqtt_topic = pub_topics_list[i]
+                _mqtt_topic = pub_topics_list[i]
                 # if the associated value in the config file,
                 # is not a <str> 'value'
                 if subs_values_list[i] != 'value' and pub_payloads_list[i] != 'payload':
                     # And if, the recieved socketio value is in the config file (expected payload)
-                    if subs_values_list[i] == protopie_value:
+                    if subs_values_list[i] == _protopie_value:
                         # then, set the mqtt pub msg as the correcponding value for that socketio value,
                         # from the config file
-                        mqtt_msg = pub_payloads_list[i]
+                        _mqtt_payload = pub_payloads_list[i]
                         pass
                 # also, if the associated socketio valkue in the config file,
                 # for that received messageid is <str> 'value',
                 # it simply means, replay the received socketio value, as it is, as the mqtt payload.
                 if subs_values_list[i] == 'value' and pub_payloads_list[i] == 'payload':
-                    mqtt_msg = protopie_value
-        if mqtt_topic is not None and mqtt_client is not None:
+                    _mqtt_payload = _protopie_value
+        if _mqtt_topic is not None and mqtt_client is not None:
             if mqtt_client.is_connected():
                 print(
-                    '[MQTT] Relaying topic:\'' + mqtt_topic +
-                    '\', message:\'' + mqtt_msg + '\' to MQTT broker')
-                mqtt_client.publish(mqtt_topic, mqtt_msg)
+                    '[MQTT] Relaying topic:\'' + _mqtt_topic +
+                    '\', message:\'' + _mqtt_payload + '\' to MQTT broker')
+                mqtt_client.publish(_mqtt_topic, _mqtt_payload)
             else:
                 print('')
                 print('Not connected to MQTT broker ...')
                 print('Hence not publishing ...')
-                print('But topic:', mqtt_topic, 'payload:', mqtt_msg)
+                print('But topic:', _mqtt_topic, 'payload:', _mqtt_payload)
                 print('')
         else:
             print('')
             print('ALERT: One of the required value for mqtt publishing is None')
-            print('Topic: ', mqtt_topic, ', payload: ', mqtt_msg)
+            print('Topic: ', _mqtt_topic, ', payload: ', _mqtt_payload)
             print('Not publishing ...')
             print('')
     else:
@@ -98,6 +92,18 @@ def on_message(data):
         print('[SOCKET_IO] Received value doesn\'t exists in our config')
         print('[SOCKET_IO] Hence not publishing anything ...')
         print('')
+
+
+@io.on('ppMessage')
+def on_message(data):
+    ''' Callback func that fires when a socketio message is received '''
+    protopie_msg_id = data['messageId']
+    protopie_value = data['value'] if 'value' in data else None
+    # [NOTE]: Both the messageId and the value received from ProtoPieConnect are apparently, ALWAYS of type <str>
+    print('[SOCKET_IO] Received a Message from ProtoPieConnect server:', protopie_msg_id, ':', protopie_value)
+    mqtt_topic = None
+    mqtt_payload = None
+    map_io(protopie_msg_id, protopie_value, mqtt_topic, mqtt_payload)
 
 
 
